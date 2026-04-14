39762c58892c0ef08b9473dc450c072946e99cde
Anderson J. Vargas R. <anderson.vargas@unicafam.edu.co>
2026-04-14 13:11:58 -0500

    Actualización de respuestas por tipo de respuesta

--- js/Scripts.js
+++ js/Scripts.js
@@ -357,9 +357,9 @@ function renderPreguntas(preguntas) {
       label.classList.add("btn", "btn-simchs", "w-100", "h-100");
       label.setAttribute("for", input.id);
       // Texto visible en el botón (Respuesta completa)
-      const respuestaCompleta = p[`R${v}`] || "";
+      const respuestaCompleta = p[`T${v}`] || "";
       // Texto del tooltip (Tipo de respuesta)
-      const tipoRespuesta = p[`T${v}`] || "";
+      const tipoRespuesta = p[`R${v}`] || "";
       // Tooltip Bootstrap
       label.setAttribute("data-bs-toggle", "tooltip");
       label.setAttribute("data-bs-placement", "top");
